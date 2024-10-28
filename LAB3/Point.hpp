0a79f1f7b8d64202516dc9e66c2ce2d3060de366
Kordian2 <bernatkordian@gmail.com>
2024-10-28 17:52:47 +0100

    Create a class in point.hpp

---
+++ LAB3/Point.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Point {
+private:
+    int m_x {};
+    int m_y {};
+public:
+    Point(int x,int y) : m_x(x), m_y(y) {
+
+    }
+    void setX(int x){
+        m_x = x;
+    }
+    void setY(int y){
+        m_y = y;
+    }
+    int getX() const {
+        return m_x;
+    }
+    int getY() const {
+        return m_y;
+    }
+    void Display(void){
+        std::cout<<"Your Point (x,y) is: ("<<getX()<<", "<<getY()<<")"<<std::endl;
+    }     
+
+};
