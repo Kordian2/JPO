36ecdc5f6180a929ddb850b8bce3d747dd382174
Kordian2 <bernatkordian@gmail.com>
2024-10-28 18:46:26 +0100

    Add some setters and getters in Circle.hpp and create zad3 for tests

--- LAB3/Circle.hpp
+++ LAB3/Circle.hpp
@@ -4,10 +4,17 @@
 class Circle{
 private:
     double *radius;
-    Point center;
+    Point m_center;
 public:
-    Circle(double r, int x, int y) : center(x,y){
+    Circle(double r, int x, int y) : m_center(x,y){
         radius = new double;
+        *radius = r;
+    }
+    int setPoint(int x, int y){
+        m_center.setX(x);
+        m_center.setY(y);
+    }
+    int setRadius(int r){
         *radius = r;
     }
 
---
+++ LAB3/zad3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Circle.hpp"
+
+int main(){
+    std::cout<<"Circle.hpp test"<<std::endl;
+    
+}
