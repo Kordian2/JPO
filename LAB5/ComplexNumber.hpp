2772c7728954237e899652685e58730fa70fc0db
Kordian2 <bernatkordian@gmail.com>
2024-11-04 19:26:08 +0100

    Create operators for complex numbers

--- LAB5/ComplexNumber.hpp
+++ LAB5/ComplexNumber.hpp
@@ -18,5 +18,47 @@ public:
     double getImag(void) const{
         return m_imag;
     }
-    
+    ComplexNumber operator+(const ComplexNumber& other) const{
+        return ComplexNumber(m_real + other.m_real,m_imag + other.m_imag);
+    }
+    ComplexNumber& operator+=(const ComplexNumber& other){
+        m_real += other.m_real;
+        m_imag += other.m_imag;
+    }
+    ComplexNumber operator-(const ComplexNumber& other) const{
+        return ComplexNumber(m_real-other.m_real,m_imag-other.m_imag);
+    }
+    ComplexNumber& operator-=(const ComplexNumber& other){
+        m_real -= other.m_real;
+        m_imag -= other.m_imag;
+    }
+    ComplexNumber operator*(const ComplexNumber& other) const{
+        return ComplexNumber(m_real*other.m_real, m_imag*other.m_imag);
+    }
+    ComplexNumber& operator*=(const ComplexNumber& other) {
+        m_real *= other.m_real;
+        m_imag *= other.m_imag;
+    }
+    ComplexNumber operator/(const ComplexNumber& other) const{
+        double denom = other.m_real*other.m_real + other.m_imag*other.m_imag;
+        double reNum = m_real*other.m_real + m_imag*other.m_imag;
+        double imNum = m_imag * other.m_real - m_real * other.m_imag;
+        return ComplexNumber(reNum/denom,imNum/denom);
+    }
+    ComplexNumber& operator /=(const ComplexNumber& other) {
+        double denom = other.m_real*other.m_real + other.m_imag*other.m_imag;
+        m_real = (m_real*other.m_real + m_imag*other.m_imag)/denom;
+        m_imag = (m_imag * other.m_real - m_real * other.m_imag)/denom;
+    }
+    bool operator==(const ComplexNumber& other) const{
+        return (m_real == other.m_real && m_imag == other.m_imag);
+    }
+    bool operator!=(const ComplexNumber& other) const{
+        return (m_real != other.m_real || m_imag != other.m_imag);
+    }
+    ComplexNumber& operator=(const ComplexNumber& other) {
+        m_real = other.m_real;
+        m_imag = other.m_imag;
+    }
+
 };
